c115aab673af224049cda8222fcfb5373e2802b0
Kamiye Oladipo <110760998+thekamiyeoladipo@users.noreply.github.com>
2026-01-15 15:47:43 +0000

    add functions for basic arithmetic and user interaction in 2.functions.py

---
+++ 2.functions.py
@@ -0,0 +1,18 @@
+def addition():
+    a = 10
+    b = 30
+    print(a + b)
+
+addition()
+
+# Getting user input
+user_name = input("Enter your name: ")
+print(f"Hello, {user_name}!")
+
+# User input in function
+def addition_():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print(f"The sum is: {num1 + num2}")
+
+addition_()
